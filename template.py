e2da95bb2248a214b2297f133b404026eb035623
gunjan <gunjankumari12082004@gmail.com>
2025-08-07 11:23:03 +0530

    zip file added

--- template.py
+++ template.py
@@ -1,12 +1,13 @@
-import os
-from pathlib import Path
-import logging
 
-logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s')
+import os 
+from pathlib import Path 
+import logging 
 
-project_name ="cnnClassifier"
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
 
-list_of_files=[
+project_name = "cnnClassifier" 
+
+list_of_files = [
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
@@ -25,22 +26,20 @@ list_of_files=[
     "templates/index.html"
 ]
 
-
 for filepath in list_of_files:
     filepath = Path(filepath)
-    filedir,filename = os.path.split(filepath)
-    
-    
-    
+    filedir, filename = os.path.split(filepath)
+
+
     if filedir !="":
-        os.makedirs(filedir,exist_ok=True)
-        logging.info(f"Creating directory: {filedir} for the file: {filename}")
-        
-        
-    if(not os.path.exists(filepath))or (os.path.getsize(filepath)==0):
-        with open(filepath,'w') as f:
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
             pass
             logging.info(f"Creating empty file: {filepath}")
+
+
     else:
-        logging.info(F"{filename} is already exits")
-    
+        logging.info(f"{filename} is already exists")
